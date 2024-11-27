0ada331772585ad87879b98c81e28abd76c2e8bd
dohanjun <dohanjun@email.com>
2024-11-27 17:48:56 +0900

    자바 시작.

---
+++ Helloworld/src/com/yedam/Hello.java
@@ -0,0 +1,15 @@
+package com.yedam;
+
+
+//자바는 대소문자 구분을 한다 (A 와 a 는 서로 다름)
+public class Hello {
+	//메인 메소드.
+	public static void main(String[] args) {
+		//콘솔에 Hello, World 출력.
+		System.out.println("Hello, World");
+		
+		// Hello.java를 컴파일하면 Hello.class(실행파일)
+		//HelloWorld 프로젝트 -HelloWorld repository
+		
+	}
+}
